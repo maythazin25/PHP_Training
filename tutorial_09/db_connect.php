8fd6358a0d5164d0b217cc157b2d461dd449b478
maythazin25 <maythazin25july@gmail.com>
2022-10-07 08:04:51 +0630

    tutorial 09

---
+++ tutorial_09/db_connect.php
@@ -0,0 +1,13 @@
+<?php
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "root";
+$dbname = "chart";
+
+// Create connection
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass);
+mysqli_select_db($conn, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
